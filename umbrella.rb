a0b0c5cf20db415022feaed3b78d3f110ca73475
mffree <matthewfreeman63@gmail.com>
2025-01-23 17:29:43 +0000

    lat and long

--- umbrella.rb
+++ umbrella.rb
@@ -4,20 +4,32 @@ require "dotenv/load" # allows you to pull in ENV vars
 require "http"
 require "json"
 
-
+# Ask user for location
 pp "Hey where are you?"
-
-# user_loc = gets
-
-user_loc = "Chicago Booth Harper Center" # hardcode a location for development purposes
-chomp at some point??
+# user_loc_raw = gets # commented out for development
+user_loc_raw = "Chicago Booth Harper Center" # hardcode a location for development purposes
+user_loc = user_loc_raw.chomp
 pp user_loc
 
+# Get lat and long from Google maps
+gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{user_loc}&key=#{ENV.fetch("GMAPS_KEY")}"
+# pp gmaps_url
 
-pp gmaps_url
+gmaps_response_string = HTTP.get(gmaps_url).to_s
+# pp gmaps_response_string
+gmaps_response_parsed = JSON.parse(gmaps_response_string)
+# pp gmaps_response_parsed
 
-pp http.get(gmaps_url)
-# pp env.fetch(GMAPS_KEY)
+# pp gmaps_response_parsed.keys
+pp gmaps_location = gmaps_response_parsed.fetch("results")[0].fetch("navigation_points")[0].fetch("location")
+lat = gmaps_location.fetch("latitude")
+pp lat
+long = gmaps_location.fetch("longitude")
+pp long
 
 
-gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{user_loc}&key=#{ENV.fetch(GMAPS_KEY)}"
+# Get weather at user's coordinates through Pirate Weather
+
+
+
+# Display current temp and summary of weather for next hour
