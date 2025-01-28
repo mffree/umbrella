5f3149a4389e65c4bff89cd3d3635c3917e4802d
mffree <matthewfreeman63@gmail.com>
2025-01-28 03:49:33 +0000

    comments

--- umbrella.rb
+++ umbrella.rb
@@ -12,16 +12,19 @@ user_loc = user_loc_raw.chomp
 pp user_loc
 
 # Get lat and long from Google maps
-gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{user_loc}&key=#{ENV.fetch("GMAPS_KEY")}"
+gmaps_api_key = ENV.fetch("GMAPS_KEY")
+gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{user_loc}&key=#{gmaps_api_key}"
 # pp gmaps_url
 
 gmaps_response_string = HTTP.get(gmaps_url).to_s
 # pp gmaps_response_string
-gmaps_response_parsed = JSON.parse(gmaps_response_string)
+gmaps_response_parsed = JSON.parse(gmaps_response_string) # parses the JSON into a Ruby Hash
 # pp gmaps_response_parsed
 
-# pp gmaps_response_parsed.keys
-pp gmaps_location = gmaps_response_parsed.fetch("results")[0].fetch("navigation_points")[0].fetch("location")
+# Getting the location from the JSON
+# Had to copy the printed gmaps_url into the browser and manually inspect the JSON to figure out how it was structured. I tried to use Envoy but I couldn't figure out how to input the Gmaps API Key
+# The hashes are inside some arrays
+pp gmaps_location = gmaps_response_parsed.fetch("results")[0].fetch("navigation_points")[0].fetch("location") # using fetch to find the values associated with specific keys in gmaps_response.parsed, which is a Hash
 lat = gmaps_location.fetch("latitude")
 pp lat
 long = gmaps_location.fetch("longitude")
@@ -29,6 +32,15 @@ pp long
 
 
 # Get weather at user's coordinates through Pirate Weather
+
+# Get API key
+# I first had to copy the API key from the Secrets page of the class website to the .env file
+pirate_weather_api_key = ENV.fetch("PIRATE_WEATHER_API_KEY")
+
+# Dig around the Pirate Weather website to find how their API is structured: https://docs.pirateweather.net/en/latest/API/
+# API request structure: https://api.pirateweather.net/forecast/[apikey]/[latitude],[longitude]
+
+CREATE MY URL
 
 
 
